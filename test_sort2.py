afda41a6e6a6cbc0c3654c01d3e1b78aff9b00c3
katieden <39695640+katieden@users.noreply.github.com>
2020-12-08 15:47:25 +0300

    demo_sort

---
+++ test_sort2.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+
+class 1(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_1(self):
+        driver = self.driver
+        driver.get("https://demo.core.heineken.com/CAWA/signin")
+        driver.find_element_by_xpath("//input[@type='text']").click()
+        driver.find_element_by_xpath("//input[@type='text']").clear()
+        driver.find_element_by_xpath("//input[@type='text']").send_keys("admin")
+        driver.find_element_by_xpath("//body[@id='main']/app-root/app-auth/div/div").click()
+        driver.find_element_by_xpath("(//input[@type='password'])[2]").click()
+        driver.find_element_by_xpath("(//input[@type='password'])[2]").clear()
+        driver.find_element_by_xpath("(//input[@type='password'])[2]").send_keys("Demo!1234")
+        driver.find_element_by_xpath("(//input[@type='password'])[2]").send_keys(Keys.ENTER)
+        driver.find_element_by_xpath(
+            "//body[@id='main']/app-root/app-main/div/div[2]/app-dashboard/div/div[6]/app-card/div").click()
+        driver.find_element_by_xpath("//span[2]").click()
+        driver.find_element_by_xpath("//li/a/span").click()
+        driver.find_element_by_xpath("//th/span").click()
+        driver.find_element_by_xpath("//th/span").click()
+        driver.find_element_by_xpath("//th[2]/span").click()
+        driver.find_element_by_xpath("//th[3]").click()
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
