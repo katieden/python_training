5e1ee38ddd2d594b28b4c6683bbb5e5e79576b2a
katieden <39695640+katieden@users.noreply.github.com>
2020-12-17 15:58:01 +0300

    создан объект Mail

---
+++ mail.py
@@ -0,0 +1,5 @@
+class Mail:
+
+    def __init__(self, address, subject):
+        self.address = address
+        self.subject = subject
--- test_mail.py
+++ test_mail.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
+from mail import Mail
 
 
 class test_mail(unittest.TestCase):
@@ -14,24 +15,24 @@ class test_mail(unittest.TestCase):
         driver = self.driver
         self.open_url(driver)
         self.login(driver, username="katieden", password="rjnktnrf.12ml")
-        self.send_mail(driver, address="katieden7@gmail.com", subject="hello2")
+        self.send_mail(driver, Mail(address="katieden7@gmail.com", subject="hello2"))
         self.logout(driver)
 
     def logout(self, driver):
         driver.find_element_by_id("PH_logoutLink").click()
 
-    def send_mail(self, driver, address, subject):
+    def send_mail(self, driver, mail):
         # open create mail
         driver.find_element_by_xpath(
             "//div[@id='app-canvas']/div/div/div/div/div[2]/span/div/div/div/div/div/div/div/div/a/span/span").click()
         # fill To field
         driver.find_element_by_xpath("(//input[@value=''])[2]").click()
         driver.find_element_by_xpath("(//input[@value=''])[2]").clear()
-        driver.find_element_by_xpath("(//input[@value=''])[2]").send_keys(address)
+        driver.find_element_by_xpath("(//input[@value=''])[2]").send_keys(mail.address)
         # fill Subject field
         driver.find_element_by_name("Subject").click()
         driver.find_element_by_name("Subject").clear()
-        driver.find_element_by_name("Subject").send_keys(subject)
+        driver.find_element_by_name("Subject").send_keys(mail.subject)
         # send mail
         driver.find_element_by_xpath("//div[2]/div/span/span/span").click()
         driver.find_element_by_xpath("//div[16]/div/div/div[2]/button/span").click()
